d9a6b4dbd75b2a2bd014d53a20a20664030b9208
Prabh 0110 <prabhupalsingh0110@gmail.com>
2024-01-26 11:03:56 -0700

    Adding function to code.py (update_1)

--- code.py
+++ code.py
@@ -3,3 +3,13 @@ b=1
 print(a-b)
 print(a+b)
 print(a*b)
+
+def calc(c,d,operation):
+    if operation=='+':
+        return (c+d);
+    elif operation=='-':
+        return (c-d);
+    elif operation == '*':
+        return (c*d);
+    else:
+        return (c/d);
